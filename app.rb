62a56fa27dc9e238156c9061c3dea03e78dee86b
Shawna Davis <shawna_davis@alum.mit.edu>
2018-10-08 16:45:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
+    get '/' do
+      "Hello, World!"
+    end
+
+    get '/name' do
+      "My name is Shawna"
+    end
+
+    get '/hometown' do
+      "My hometown is Philly"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is Bood Up"
+    end
+
+
+
 
 end
